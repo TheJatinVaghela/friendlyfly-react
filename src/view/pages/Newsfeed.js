635237f205d3aa4c2fb45a4736a8c75e009ad44e
loljaar <vaghelajatin5@example.com>
2023-11-24 13:54:09 +0530

    hm

--- src/view/pages/Newsfeed.js
+++ src/view/pages/Newsfeed.js
@@ -14,11 +14,7 @@ function Newsfeed(props) {
                     <span>Add Stories</span>
                 </div>
             </div>
-            <div className='otherUserStorie-1 story-style'>
-                <div className='otherUserStorie-1-wrapper story-wrapper-style'>
-                    <img src=""/>
-                </div>
-            </div> 
+            <Stories/>
         </section>
         <section className='newsfeed-main'>
             <NewsfeedMainLeft/>
@@ -33,7 +29,25 @@ function Newsfeed(props) {
 
 Newsfeed.propTypes = {}
 
-
+function Stories(params) {
+  
+  return(
+    <div className='otherUserStorie-1 story-style'>
+      <div className='followings-stories-overlay story-wrapper-style'
+      style={{
+        "background": "-webkit-gradient(linear, left bottom, left top, from(#e18d1e), to(rgba(0, 212, 255, 0)))", 
+        "background": "linear-gradient(0deg, #e18d1e 0%, rgba(0, 212, 255, 0) 100%)"
+      }}  
+      >
+      </div>
+      <div className='otherUserStorie-1-wrapper story-wrapper-style followings-stories'
+      style={{"background-image": "url(https://themes.pixelstrap.com/friendbook/assets/images/story/6.jpg)"}}>
+        <span>User Name</span>
+        <span>Active Now</span>
+      </div>
+    </div> 
+  );
+}
 function NewsfeedMainLeft(props) {
   return (
     <div className='newsfeed-main-left'>
